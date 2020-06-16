2f14d9bfffef9f3e97a101de2ff034ef9b8f5b28
C-Wwb <1729220020@qq.com>
2020-06-16 09:26:21 +0800

    20.06.16

---
+++ simple/1304/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Solution solution = new Solution();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a number: ");
+        int n = input.nextInt();
+        System.out.println(Arrays.toString(solution.sumZero(n)));
+    }
+}
+class Solution {
+    public int[] sumZero(int n) {
+        int[] result = new int[n];
+        int sum = 0;
+        result[0] = 0;
+        for(int i = 1; i < n - 1; i++)
+        {
+            result[i] += result[i - 1] + 1;
+            sum += result[i];
+        }
+        result[n - 1] = - sum;
+        return result;
+    }
+}
