ab93d02ed76e52816b9a38ab24049338b5da8c57
C-Wwb <1729220020@qq.com>
2020-06-23 17:45:05 +0800

    20.06.23

---
+++ simple/67/src/Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Solution solution = new Solution();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the string1: ");
+        String a = input.next();
+        System.out.println("Please enter the string2: ");
+        String b = input.next();
+        System.out.println(solution.addBinary(a, b));
+    }
+}
+/*class Solution {
+    public String addBinary(String a, String b) {
+        return Integer.toBinaryString(Integer.parseInt(a, 2) + Integer.parseInt(b, 2));
+    }
+}*/
+class Solution {
+    public String addBinary(String a, String b) {
+        StringBuffer ans = new StringBuffer();
+
+        int n = Math.max(a.length(), b.length()), carry = 0;
+        for (int i = 0; i < n; ++i) {
+            carry += i < a.length() ? (a.charAt(a.length() - 1 - i) - '0') : 0;
+            carry += i < b.length() ? (b.charAt(b.length() - 1 - i) - '0') : 0;
+            ans.append((char) (carry % 2 + '0'));
+            carry /= 2;
+        }
+
+        if (carry > 0) {
+            ans.append('1');
+        }
+        ans.reverse();
+
+        return ans.toString();
+    }
+}
