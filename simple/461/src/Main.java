6bb0ab80d3f15868c109de3b01dadf5a163d6fae
C-Wwb <1729220020@qq.com>
2020-07-25 11:33:35 +0800

    20.07.25

---
+++ simple/461/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Solution solution = new Solution();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter number1: ");
+        int x = input.nextInt();
+        System.out.println("Please enter number2: ");
+        int y = input.nextInt();
+        System.out.println(solution.hammingDistance(x, y));
+    }
+}
+
+class Solution {
+    public int hammingDistance(int x, int y) {
+        return Integer.bitCount(x ^ y);
+    }
+}
