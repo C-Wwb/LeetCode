1d9c1109543a3d08450655179ff2946694fa539c
C-Wwb <1729220020@qq.com>
2020-09-01 10:19:22 +0800

    20.09.01

---
+++ simple/1450/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int[] startTime = {4};
+        int[] endTime = {4};
+        int queryTime = 4;
+        Solution solution = new Solution();
+        System.out.println(solution.busyStudent(startTime, endTime, queryTime));
+    }
+}
+class Solution {
+    public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+        int count = 0;
+        for (int i = 0; i < startTime.length; i++)
+        {
+            if (startTime[i] <= queryTime && endTime[i] >= queryTime)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
